0c6fa44528e2d372a09e212b1581aff5ef7768b2
Lavínia Melo <m0rgan7@users.noreply.github.com>
2026-05-05 09:49:15 -0300

    modified:   Easy/Statistics and Machine Learning/Laptop Battery Life/code.py
    modified:   Medium/Statistics and Machine Learning/code.py

--- Easy/Statistics and Machine Learning/Laptop Battery Life/code.py
+++ Easy/Statistics and Machine Learning/Laptop Battery Life/code.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     timeCharged = float(input('Enter the time charged: ').strip())
 
     if timeCharged < 4:
-        print(f"{2 * timeCharged:.2f} hours")
+        print(f"{2 * timeCharged:.2f} hours") #twice cuz of the training data, it goes like (5.62 / 2.81 = 2), (3.80 / 1.90 = 2)...
+
     else:
         print("8.00 hours")
--- Medium/Statistics and Machine Learning/code.py
+++ Medium/Statistics and Machine Learning/code.py
@@ -1,1 +1,19 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import math
+
+physics = [15,12,8,8,7,7,7,6,5,3]
+history = [10,25,17,11,13,17,20,13,9,15]
+
+n = len(physics)
+
+mean_x = sum(physics)/n
+mean_y = sum(history)/n
+
+num = sum((x - mean_x)*(y - mean_y) for x, y in zip(physics, history))
+den_x = sum((x - mean_x) ** 2 for x in physics)
+den_y = sum((y - mean_y)** 2 for y in history)
+
+r = num/math.sqrt(den_x * den_y)
+
+print(f"{r:.3f}")
